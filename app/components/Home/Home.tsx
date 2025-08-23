78f1b0b9e6d527660213ba251e417c91e0b817fe
AL ZAYAN P <alzayanpoothakkal1156@gmail.com>
2025-08-23 21:49:06 +0530

    serivice section

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero/Hero';
 import About from './About/About';
+import Services from './Services/Services';
 
 
 const Home = () => {
   return  <div className='overflow-hidden '>
     <Hero/>
     <About/>
+    <Services/> 
   </div>
   
 };
---
+++ app/components/Home/Services/Services.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Services = () => {
+  return <div className=' pt-16 pb-16'> 
+  <h1 className=' text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white'>Services</h1>
+  </div>
+
+}
+
+export default Services;
