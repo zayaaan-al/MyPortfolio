21446bdd3fc42f2c59b0e70ba6cc8fffd06e0d93
AL ZAYAN P <alzayanpoothakkal1156@gmail.com>
2025-08-27 07:01:24 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // ❌ Don’t block builds on ESLint errors
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
