6bf340465c2bdabe94daef96781cc11305294559
AL ZAYAN P <alzayanpoothakkal1156@gmail.com>
2025-10-06 19:38:29 +0530

    updated

--- app/components/Home/Navbar/MobileNav.tsx
+++ app/components/Home/Navbar/MobileNav.tsx
@@ -3,36 +3,46 @@ import Link from 'next/link';
 import React from 'react';
 import { CgClose } from 'react-icons/cg';
 
-type props ={
+type Props = {
   showNav: boolean;
-  closeNav:()=>void;
-}
+  closeNav: () => void;
+};
 
-const MobileNav = ({closeNav,showNav} :props) => {
-
-
+const MobileNav = ({ closeNav, showNav }: Props) => {
   const navOpen = showNav ? "translate-x-0" : "translate-x-[100%]";
 
-  
-  return <div>
-    {/* {overlay} */}
-    <div className={`fixed inset-0 ${navOpen} transform transition-all right-0 duration-500 z-[100002] bg-black opacity-70 w-full h-screen`}></div>
-    {/* {navLinks} */}
-    <div className={`text-white ${navOpen} fixed justify-center  flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%]
-    bg-cyan-800 space-y-6 z-[100050] right-0`}>
-      {NavLinks.map((link) =>{
-        return <Link key={link.id} href={link.url}>
-          <p className='text-white w-fit text-xl ml-12 border-b-[1.5px] pb-1  border-white  sm:text-[30px]
-          '>{link.Label}</p>
-        </Link>
-      })}
-       {/* {close icon} */}
-    <CgClose onClick={closeNav} className='absolute top-[0.7rem] right-[1.4rem] sm:h-8 sm:w-8 w-6 h-6' />
+  return (
+    <div>
+      {/* Overlay */}
+      <div
+        className={`fixed inset-0 ${navOpen} transform transition-all right-0 duration-500 z-[100002] bg-black opacity-70 w-full h-screen`}
+        onClick={closeNav}
+      ></div>
+
+      {/* Navigation Drawer */}
+      <div
+        className={`text-white ${navOpen} fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%]
+        bg-cyan-800 space-y-6 z-[100050] right-0`}
+      >
+        {/* Close Icon */}
+        <CgClose
+          onClick={closeNav}
+          className="absolute top-[0.7rem] right-[1.4rem] sm:h-8 sm:w-8 w-6 h-6 cursor-pointer"
+        />
+
+        {/* Nav Links */}
+        {NavLinks.map((link) => (
+          <Link key={link.id} href={link.url} onClick={closeNav}>
+            <p
+              className="text-white w-fit text-xl ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px] hover:text-cyan-300 transition-colors duration-200"
+            >
+              {link.Label}
+            </p>
+          </Link>
+        ))}
+      </div>
     </div>
-   
-
-  </div>
-
-}
+  );
+};
 
 export default MobileNav;
